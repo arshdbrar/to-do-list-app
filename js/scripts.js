e5f95a16d51d3bb4d4b6e31cf91fa07332e8ae50
arshdbrar <139406297+arshdbrar@users.noreply.github.com>
2023-08-22 14:46:25 -0700

    update to jQuery code

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,30 @@
 function newItem() {
-
     //1. Adding a new item to the list of items: 
-
     let li = $('<li></li>');
-
     let inputValue = $('#input').val();
-
     li.append(inputValue);
 
-
     if (inputValue === '') {
-
         alert("You must write something!");
-
     } else {
-
         $('#list').append(li);
-
     }
-
 
     function crossOut() {
-
-        li.classList.toggle("strike");
-
+        li.toggleClass("strike");
     }
-
-
     li.on("dblclick", function crossOut() {
-
-        li.classList.toggle("strike");
-
+        li.toggleClass("strike");
     });
-
-
-    //2. Reordering the items: 
-
-    $('#list').sortable();
-
 }
 
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append(document.createTextNode('X'));
+li.append(crossOutButton);
+
+crossOutButton.on("click", deleteListItem);
 function deleteListItem() {
-
-    li.addClass("delete");
-
+    li.addClass("delete")
 }
 
 $('#list').sortable();
